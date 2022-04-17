f5337d4890204c9e0d133927ebeeb01f1934fc64
Евгений Сергеев <alexevgenii@mail.ru>
2022-04-17 14:27:34 +0400

    fixxxxxxxxxxxx

--- AbstractFoodDeliveryDatabaseImplement/AbstractFoodDeliveryDatabase.cs
+++ AbstractFoodDeliveryDatabaseImplement/AbstractFoodDeliveryDatabase.cs
@@ -9,7 +9,7 @@ namespace AbstractFoodDeliveryDatabaseImplement
         {
             if (optionsBuilder.IsConfigured == false)
             {
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-FQA7MOU\SQLEXPRESS;Initial Catalog=AbstractFoodDeliveryDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+                optionsBuilder.UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=AbstractFoodDeliveryDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
             }
             base.OnConfiguring(optionsBuilder);
         }
